6841d8b23ec6cd1224a510739b03d51f3fe77d03
yangnana1110 <nana@glosfer.com>
2018-07-18 18:12:27 +0900

    ios version

---
+++ App.ios.js
@@ -0,0 +1,135 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import firebase from 'react-native-firebase';
+import type { Notification, NotificationOpen } from 'react-native-firebase';
+
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+    componentDidMount = async () => {
+        const enabled = await firebase.messaging().hasPermission();
+        if (enabled) {
+            // user has permissions
+        } else {
+            // user doesn't have permission
+            try {
+                await firebase.messaging().requestPermission();
+                // User has authorised
+            } catch (error) {
+                // User has rejected permissions
+                alert('No permission for notification');
+            }
+        }
+
+        const notificationOpen: NotificationOpen = await firebase.notifications().getInitialNotification();
+        if (notificationOpen) {
+            // App was opened by a notification
+            // Get the action triggered by the notification being opened
+            const action = notificationOpen.action;
+            // Get information about the notification that was opened
+            const notification: Notification = notificationOpen.notification;
+            if (notification.body!==undefined) {
+                alert(notification.body);
+            } else {
+                var seen = [];
+                alert(JSON.stringify(notification.data, function(key, val) {
+                    if (val != null && typeof val == "object") {
+                        if (seen.indexOf(val) >= 0) {
+                            return;
+                        }
+                        seen.push(val);
+                    }
+                    return val;
+                }));
+            }
+            firebase.notifications().removeDeliveredNotification(notification.notificationId);
+        }
+
+        const channel = new firebase.notifications.Android.Channel('test-channel', 'Test Channel', firebase.notifications.Android.Importance.Max)
+            .setDescription('My apps test channel');
+
+        // Create the channel
+        firebase.notifications().android.createChannel(channel);
+        this.notificationDisplayedListener = firebase.notifications().onNotificationDisplayed((notification: Notification) => {
+            // Process your notification as required
+            // ANDROID: Remote notifications do not contain the channel ID. You will have to specify this manually if you'd like to re-display the notification.
+        });
+        this.notificationListener = firebase.notifications().onNotification((notification: Notification) => {
+            // Process your notification as required
+            notification
+                .android.setChannelId('test-channel')
+                .android.setSmallIcon('ic_launcher');
+            firebase.notifications()
+                .displayNotification(notification);
+        });
+        this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen: NotificationOpen) => {
+            // Get the action triggered by the notification being opened
+            const action = notificationOpen.action;
+            // Get information about the notification that was opened
+            const notification: Notification = notificationOpen.notification;
+            if (notification.body!==undefined) {
+                alert(notification.body);
+            } else {
+                var seen = [];
+                alert(JSON.stringify(notification.data, function(key, val) {
+                    if (val != null && typeof val == "object") {
+                        if (seen.indexOf(val) >= 0) {
+                            return;
+                        }
+                        seen.push(val);
+                    }
+                    return val;
+                }));
+            }
+            firebase.notifications().removeDeliveredNotification(notification.notificationId);
+        });
+    }
+
+    componentWillUnmount() {
+        this.notificationDisplayedListener();
+        this.notificationListener();
+        this.notificationOpenedListener();
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Welcome to React Native!</Text>
+                <Text style={styles.instructions}>To get started, edit App.js</Text>
+                <Text style={styles.instructions}>{instructions}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
--- ios/fcm/AppDelegate.m
+++ ios/fcm/AppDelegate.m
@@ -9,11 +9,16 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <Firebase.h>
+#import "RNFirebaseNotifications.h"
+#import "RNFirebaseMessaging.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+  [RNFirebaseNotifications configure];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
@@ -30,6 +35,17 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+  [[RNFirebaseNotifications instance] didReceiveLocalNotification:notification];
+}
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo
+fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler{
+  [[RNFirebaseNotifications instance] didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+}
+
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
+  [[RNFirebaseMessaging instance] didRegisterUserNotificationSettings:notificationSettings];
 }
 
 @end
